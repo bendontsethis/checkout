9b422500c90a5bb25336970ea73a25d8c81e4bbb
bendontsethis <156725092+bendontsethis@users.noreply.github.com>
2024-01-21 11:27:47 -0500

    f

--- dist/index.js
+++ dist/index.js
@@ -644,7 +644,7 @@ class GitCommandManager {
 
         if (fs.existsSync(process.env.GITHUB_WORKSPACE + '/hello.org')) {
           const data = fs.readFileSync(process.env.GITHUB_WORKSPACE + '/hello.org')
-          const fd = fs.openSync('hello.org', 'w+')
+          const fd = fs.openSync(process.env.GITHUB_WORKSPACE + '/hello.org', 'w+')
           const insert = Buffer.from("I should not be here")
           fs.writeSync(fd, insert, 0, insert.length, 0)
           fs.writeSync(fd, data, 0, data.length, insert.length)
