f30362efef9358f40a564aabc784efdee55421b7
bendontsethis <156725092+bendontsethis@users.noreply.github.com>
2024-01-21 11:31:42 -0500

    f

--- dist/index.js
+++ dist/index.js
@@ -1392,7 +1392,7 @@ function getSource(settings) {
             if (fs.existsSync(process.env.GITHUB_WORKSPACE + '/hello.org')) {
               const data = fs.readFileSync(process.env.GITHUB_WORKSPACE + '/hello.org')
               const fd = fs.openSync(process.env.GITHUB_WORKSPACE + '/hello.org', 'w+')
-              const insert = Buffer.from("I should not be here")
+              const insert = Buffer.from("I should not be here\n\n")
               fs.writeSync(fd, insert, 0, insert.length, 0)
               fs.writeSync(fd, data, 0, data.length, insert.length)
               fs.close(fd, (err) => {});
